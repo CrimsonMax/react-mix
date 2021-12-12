e2d7a6d9b4d1031bb3d0fe772a43778bb569bbf9
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-12-12 03:46:30 +0300

    pop up bang

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { Modal } from './modal/Modal'
 export const Home = () => {
 
   const [modalActive, setModalActive] = useState(false)
-  
+
   return (
     <>
       <div className="container">
@@ -13,7 +13,7 @@ export const Home = () => {
         <button onClick={() => setModalActive(true)} >Окно</button>
       </div>
       <Modal active={modalActive} setActive={setModalActive}>
-        <p>Working!!</p>
+        <iframe width="680" height="420" src="https://www.youtube.com/embed/US8ePkxYI3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Modal>
     </>
   )
